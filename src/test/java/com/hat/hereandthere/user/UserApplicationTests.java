2d238fbc36fe5ded1d6307b86c77b313f1445649
10000001a <pgdn06288888@gmail.com>
2024-04-16 22:10:44 +0900

    FEAT: 프로젝트 생성

---
+++ src/test/java/com/hat/hereandthere/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hat.hereandthere.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
